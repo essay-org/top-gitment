9e361e6756c0c3f4a4299acc85275b66635ab223
wmui <qq22337383@gmail.com>
2017-09-24 14:03:05 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <vue-gitment :options="options" v-if="options"></vue-gitment>
+  </div>
+  </div>
+</template>
+<script>
+import VueGitment from './components/VueGitment.vue'
+export default {
+  data() {
+    return {
+      options: {
+        id: 'article id',
+        owner: 'Your GitHub ID',
+        repo: 'The repo to store comments',
+        oauth: {
+          client_id: 'Your client ID', 
+          client_secret: 'Your client secret',
+        } 
+      }
+    }
+  },
+  components: {
+    VueGitment
+  }
+}
+</script>
+<style>
+	@import '~gitment/style/default.css';
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App.vue'
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
+/*import VueGitment from './components/VueGitment.vue'
+export default {
+  install(vue) {
+    Vue.component('vue-gitment', VueGitment)
+  }
+}*/
